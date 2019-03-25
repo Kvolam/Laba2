75f4c725ac3a61e86e20382c6d8141b5c04e5dcb
Kvolam <47974303+Kvolam@users.noreply.github.com>
2019-03-25 13:49:04 +0300

    Delete Sugar.java

--- src/Sugar.java
+++
@@ -1,8 +0,0 @@
-public class Sugar extends Thing{
-    public Sugar(Integer Weight) {
-        super(Weight);
-    }
-    public void ToPourSugar(Cup CupOfCoffee){
-        CupOfCoffee.ToFill(this.getWeight(),"Sugar");
-    }
-}
